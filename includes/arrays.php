22f5a8d595fa269fa2da3e287c0b0b374ff3222d
Stephan Yu <Stephan.yu@gmail.com>
2015-02-24 13:56:57 +0100

    added dish.php and menu.php pages

--- includes/arrays.php
+++ includes/arrays.php
@@ -42,4 +42,50 @@
       img => "carlos"
     )
   );
+
+  // Menu Items
+  $menuItems = array(
+    'club-sandwich' => array(
+      title => "Club Sandwich",
+      price => 11,
+      blurb => "Bacon ipsum dolor amet cupim pork chicken meatball. Fatback short ribs short loin, t-bone ham meatball beef ribs. Short loin pork belly shank shoulder. Bacon short loin ground round drumstick. Shank venison landjaeger salami jowl leberkas fatback, tail ground round tri-tip cupim pork loin. Short loin ribeye chicken landjaeger, shankle pork chop biltong venison.",
+      drink => "Club Soda"
+    ),
+    'dill-salmon' => array(
+      title => "Lemon &amp; Dill Salmon",
+      price => 18,
+      blurb => "Minim shank pork loin excepteur, exercitation kevin in. Corned beef turducken jerky filet mignon incididunt proident id pork shankle drumstick. Biltong eiusmod ham hock, frankfurter aute pork chop beef porchetta dolor rump. Officia irure ham beef doner turducken deserunt ribeye tri-tip incididunt lorem.",
+      drink => "Red Wine"
+    ),
+    "super-salad" => array(
+      title => "The Super Salad<sup>&reg;</sup>",
+      price => 34,
+      blurb => "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.",
+      drink => "Jug o' Water"
+    ),
+    "mexican-barbacoa" => array(
+      title => "Mexican Barbacoa",
+      price => 23,
+      blurb => "I love fruitcake danish caramels. Tart danish pastry liquorice chocolate cake fruitcake. Bear claw gingerbread muffin I love apple pie apple pie tiramisu brownie chocolate. Sweet roll cotton candy cupcake gingerbread gummies jelly-o. Muffin I love croissant I love jelly-o brownie jelly beans. Toffee I love pastry.",
+      drink => "Beer with a lime"
+    ),
+  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
